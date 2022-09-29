efd2a011862f17f30d8a68bec809bb18d377dad2
Ethan <ethan.laudick24@d155.org>
2022-09-29 07:45:34 -0500

    Initial Commit

---
+++ clickerGameViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerGameViewControllers
+//
+//  Created by ETHAN LAUDICK on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
